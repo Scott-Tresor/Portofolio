b82b6d613dd653f7128fc93c7ffcd4ab80edc368
scott-Tresor <scotttresor@gmail.com>
2020-06-25 14:52:48 +0200

    feat:ajout de l'api avec adonisJS

--- api/start/routes.js
+++ api/start/routes.js
@@ -6,7 +6,7 @@
 |--------------------------------------------------------------------------
 |
 | Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
+| routes for different URLs and bind Controller actions to them.
 |
 | A complete guide on routing is available here.
 | http://adonisjs.com/docs/4.1/routing
@@ -14,7 +14,8 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route');
+const Route = use('Route')
 
-Route.post('/contact', 'ContactController.store');
-Route.get('/contact', 'ContactController.index');
+Route.get('/', () => {
+  return { greeting: 'Hello world in JSON' }
+})
