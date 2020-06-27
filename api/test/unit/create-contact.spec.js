17e23962365657575206e8925394bb36b790f79b
scott-Tresor <scotttresor@gmail.com>
2020-06-27 16:35:05 +0200

    feat:finish frontend

--- api/test/unit/create-contact.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const { test } = use('Test/Suite')('Create Contact')
-
-test('can create a challenge if valid data', async({ assert }) => {
-  const response = // do api call
-
-  response.assertStatus(201);
-})
